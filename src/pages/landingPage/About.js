e15606265ebe781acfa5e01eadbde6e534eb3b13
hanifnabila <hanifnabila10@gmail.com>
2023-06-21 13:42:08 +0700

    update: about

--- src/pages/landingPage/About.js
+++ src/pages/landingPage/About.js
@@ -8,21 +8,25 @@ const About = () => {
             </div>
             <div className='border-solid border-2 border-purple-400 rounded-lg w-[670px] h-[350px] mt-12 drow-shadow-lg top-[140px] flex items-center justify-center'>
                 <div className='w-1/2 pl-6'>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt quasi, inventore ipsam voluptas, ducimus facilis esse quis quidem autem, unde id! Pariatur officia saepe facilis natus non harum omnis inventore.</p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum minima doloremque possimus vitae consequuntur beatae facilis sequi incidunt rem ut harum optio aliquam repellat, ratione voluptate quia ex nesciunt ullam, quibusdam vel accusantium similique. Sequi possimus praesentium vero, qui velit voluptate est, omnis rem minima, ad officia. Impedit, cupiditate officia.
                 </div>
-                <div className='w-1/2 flex flex-col justify-center'>
+                <div className='w-1/2 flex flex-col'>
                     <div className='absolute bg-green-400 w-[150px] h-[50px] bottom-[110px] rounded-lg flex items-center'>
-                        <ion-icon name="people"></ion-icon>
-                        <div className='flex flex-col items-center'>
-                            <div className='font-bold text-[19px]'>36</div>
-                            <div className='font-bold text-[19px]'>Santri</div>
+                        <div className='ml-3 text-4xl'>
+                            <ion-icon name="people"></ion-icon>
+                        </div>
+                        <div className='flex flex-col items-center ml-4'>
+                            <div className='font-bold text-[18px] pt-2'>36 Orang</div>
+                            <div className='text-[19px] pb-2'>Santri</div>
                         </div>
                     </div>
                     <div className='absolute ml-40 bg-yellow-400 w-[150px] h-[50px] bottom-[110px] rounded-lg flex items-center'>
-                        <ion-icon name="people"></ion-icon>
-                        <div className='flex flex-col items-center'>
-                            <div className='font-bold text-[19px]'>3</div>
-                            <div className='font-bold text-[19px]'>Pengajar</div>
+                        <div className='ml-3 text-4xl'>
+                            <ion-icon name="people"></ion-icon>
+                        </div>
+                        <div className='flex flex-col items-center ml-4'>
+                            <div className='font-bold text-[18px]'>3 Orang</div>
+                            <div className='text-[19px]'>Pengajar</div>
                         </div>
                     </div>
                 </div>
