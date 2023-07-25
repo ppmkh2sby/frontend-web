c6b05f66954408656d252dfee2ad5d70ecf7cb8a
hanifnabila <hanifnabila10@gmail.com>
2023-07-25 14:32:05 +0700

    update : link to

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import profile from "../assets/image/profil.jpg";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 const Sidebar = () => {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
   return (
     <div className="z-10 w-56 h-full bg-green-400">
       <div className="flex flex-col px-6 py-4">
@@ -21,20 +22,21 @@ const Sidebar = () => {
         </div>
         <ul className="sidebar-nav mt-6">
           <li className="sidebar-item flex items-center my-2">
-            <p onClick={() => navigate('/dashboard')} className="flex items-center text-base">
+            <p className="flex items-center text-base">
               <ion-icon name="grid"></ion-icon>
-              <span className="ml-3">Dashboard</span>
+              <span className="ml-3"><Link to='/admin'>Dashboard</Link></span>
             </p>
             <div className="flex-grow"></div>
             <ion-icon name="arrow-dropdown"></ion-icon>
           </li>
           <li className="sidebar-item flex items-center my-2">
-            <p onClick={() => navigate('/dashboard/user')} className="flex items-center text-base">
+            <p className="flex items-center text-base">
               <ion-icon name="people"></ion-icon>
-              <span className="ml-3">Users</span>
+              <span className="ml-3"><Link to='/user'>Users</Link></span>
             </p>
           </li>
         </ul>
+        <Outlet />
       </div>
     </div>
   );
